6c51ea24edafd92b337f8c86376f95cbf170fd2f
Cuza-ony <co80389@uga.edu>
2016-12-04 11:50:47 -0500

    Add files via upload

--- cuza_uploads/interests.js
+++ cuza_uploads/interests.js
@@ -1,13 +1,21 @@
-/**
- * 
+/** Cuza Onyeagba
+ * For interest.html
+ * ajax request gets list of all interests and user specific interest 
+ * showCurrent places interests in proper place in interest.html
  */
 "use strict";
+var value;
+var valueParts;
 $(function() {
-	$.ajax({ url: "Servlet", data: "p=interests",success: showCurrent});
+	$.ajax({ url: "Servlet", data: "p=interests",success: showInterests});
 });
 //Change the welcome panel to match the name of the searched club
-function showCurrent(info){
-	    alert(info);
+function showInterests(info){
+	value = info; 
+    valueParts = value.split("{.split}");
+    //place html in there proper places on page
+    $("#newArea").html(valueParts[0]);
+    $("#currArea").html(valueParts[1]);
 }
 
 
