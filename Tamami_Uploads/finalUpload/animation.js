51e987a9035c18729393dcd5f169be3ac471a2a9
trlillis <tlillis@uga.edu>
2016-12-06 17:19:34 -0500

    Add files via upload

---
+++ Tamami_Uploads/finalUpload/animation.js
@@ -0,0 +1,10 @@
+/**
+ * JS page to add functionality to User Login/Register Page
+ * After User successfully Logs in or creates an account they will go to user landing page
+ */
+$('.message a').click(function(){
+   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+});
+
+
+
